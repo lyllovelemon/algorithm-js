88d6f0509275cceebffcf7eccdbe322b5b30ffb1
lyllovelemon <732193462@qq.com>
2019-03-29 17:17:55 +0800

    append linklist and doublylinkedlist

---
+++ linkList/doublyLinkedList/doublyLinkedList.js
@@ -0,0 +1,28 @@
+function DoublyLinkedList() {
+    let node=function (element) {
+        this.element=element;
+        this.next=null;
+        this.prev=null;
+    }
+    let length=0;
+    let head=null;
+    let tail=null;
+
+    this.insert=function (position,element) {
+        if(position>=0 && position<=length){
+            let node=new Node(),current=head,previous,index=0;
+            if(position===0){
+                if(!head){
+                    head=node;
+                    tail=node;
+                }
+                else {
+                    node.next=current;
+                    current.prev=node;
+                    head=node;
+                }
+            }
+            else if(){}
+        }
+    }
+}
--- linkList/linkList.js
+++ linkList/linkList.js
@@ -24,14 +24,82 @@ function LinkedList() {
     }
     length++;//更新链表长度
     };
-    this.insert=function (position,element) {};//向列表特定位置插入一个新项
-    this.removeAt=function (position) {};
-    this.remove=function (element) {};
-    this.indexOf=function (element) {};
-    this.isEmpty=function () {};
-    this.size=function () {};
-    this.toString=function () {};
+    //向列表特定位置插入一个新项
+    this.insert=function (position,element) {
+      if(position>0 && position<=length){
+          let node=new Node(element),current=head,previous,index=0;
+          if(position===0){
+              node.next=current;
+              head=node;
+          }
+          else {
+              while (index++<position){
+                  previous=current;
+                  current=current.next;
+              }
+              node.next=current;
+              previous.next=node;
+          }
+          length++;
+          return true;
+      }
+      else {
+          return false;
+      }
+    };
+    this.removeAt=function (position) {
+        if(position>-1 && position<length){
+            let current=head,previous,index=0;
+            if(position===0){
+                head=current.next;
+            }
+            else{
+                while (index++<position){
+                    previous=current;
+                    current=current.next;
+                }
+                previous.next=current.next;
+            }
+            length--;
+            return current.element;
+        }
+        else{
+            return null;
+        }
+    };
+    this.remove=function (element) {
+        let index=this.indexOf(element);
+        return this.removeAt(index);
+    };
+    this.indexOf=function (element) {
+        let current=head,index=-1;
+        while (current){
+            if(element===current.element){
+                return index;
+            }
+            index++;
+            current=current.next;
+        }
+        return -1;
+    };
+    this.isEmpty=function () {
+        return length===0;
+    };
+    this.size=function () {
+        return length;
+    };
+    this.toString=function () {
+        let current=head,string='';
+        while (current){
+            string=current.element;
+            current=current.next;
+        }
+        return string;
+    };
     this.print=function () {};
+    this.getHead=function () {
+        return head;
+    }
 }
 let list=new LinkedList();
 list.append(15);
