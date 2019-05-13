4775b051bc0248b6c301630f2d4638f79f198a0e
liyili <732193462@qq.com>
2019-05-13 17:17:26 +0800

    feat:add para

--- algorithm/sort.js
+++ algorithm/sort.js
@@ -2,8 +2,9 @@
  * Created by admin on 2019/4/24.
  * @author lemon<李亦黎>
  */
-function Array() {
-	this.sort=function (a,b) {
-		return a-b;
-	}
-}
+
+// function Array() {
+// 	this.sort=function (a,b) {
+// 		return a-b;
+// 	}
+// }
