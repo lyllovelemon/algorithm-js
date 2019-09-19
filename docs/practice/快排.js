b9ba2659a70b6fc14d066d3591dd4b878aacb259
lyllovelemon <732193462@qq.com>
2019-09-19 23:08:40 +0800

    feat:复习快排

---
+++ docs/practice/快排.js
@@ -0,0 +1,23 @@
+/**
+ * 快速排序是冒泡排序的改进
+ * 思路:
+ * 1.在数组里找到一个基准数字pivot
+ * 2.循环遍历数组，值比pivot小则存入左边的数组，否则存入右边的数组
+ * 3. 不断重复步骤2，直到只剩一个元素
+ * **/
+function qucikSort(arr) {
+    if(arr.length<=1){ return arr;}
+    let pivotIndex=Math.floor(arr.length/2);
+    let pivot=arr.splice(pivotIndex,1)[0];
+    let left=[];
+    let right=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<pivot){
+            left.push(arr[i])
+        }
+        else {
+            right.push(arr[i])
+        }
+    }
+    return  qucikSort(left).concat([pivot],qucikSort(right))
+}
